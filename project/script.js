d32f8b107ea0dc3208f8f5d12ff082176a6a6a2b
ana <ana@anniecannons.com>
2023-09-12 11:11:37 -0700

    added the if statment for the weathermessages and fixed the template literal so i could get the data and then appened the data to show on the page and added some css

--- project/script.js
+++ project/script.js
@@ -1,38 +1,77 @@
+// Keep the same variable and function names
 const searchButton = document.getElementById("searchButton");
 const cityInput = document.getElementById("cityInput");
+const afterSearchWeather = document.getElementById("afterSearchWeather");
 
- const API_KEY = "81c688300b9f4a07b49194332231109";
- 
-
+const API_KEY = "81c688300b9f4a07b49194332231109";
 
 function inputCityName() {
   searchButton.addEventListener("click", function () {
-    console.log("this is clicked");
+    console.log("Button clicked");
     const cityName = cityInput.value;
-    console.log("cityName", cityName);
+    console.log("City Name:", cityName);
     getWeather(cityName);
   });
 }
+
 inputCityName();
 
 async function getWeather(cityName) {
   try {
-    // You can use cityName and API_KEY to make an asynchronous API request here
     const response = await fetch(
-        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}`
+      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}`
     );
-    console.log(response, "response")
-    const data = await response.json();
-    console.log(data, "data")
 
-    const city = data.location.name;
-   const country = data.location.country;
-   const region = data.location.region;
-
-
-
-   
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data, "data");
+      displayWeather(data);
+    } else {
+      console.error("Failed to fetch weather data");
+    }
   } catch (error) {
     console.error("An error occurred:", error);
   }
+}
+
+function displayWeather(data) {
+  if (!data) {
+    return;
+  }
+  const cityName = data.location.name;
+  console.log(cityName, "cityName");
+  const country = data.location.country;
+  console.log(country, "country");
+  const currentTemperature = data.current.temp_f;
+  console.log(currentTemperature, "currentTemperature");
+  const weatherIcon = data.current.condition.icon;
+  console.log(weatherIcon, "weatherIcon");
+  const weatherMessage = data.current.condition.text;
+  console.log(weatherMessage, "weatherMessage");
+
+  const weatherDataTemplate = `
+        <h2 id="cityNameCountry">${cityName}, ${country}</h2>
+        <p id="currentTemp">Current Temperature: ${currentTemperature}°F</p>
+        <p id="highTemp"></p>
+        <p id="lowTemp"></p>
+        <p id="precipitation"></p>
+        <p id="weatherWarningMessage">${weatherIcon}</p>
+        <p id="textIcon">${weatherMessage}</p>
+    `;
+
+  console.log(weatherDataTemplate, "weatherDataTemplate");
+
+  afterSearchWeather.innerHTML = weatherDataTemplate;
+
+  const temperatureMessageElement = document.createElement("p");
+  if (currentTemperature > 75) {
+    temperatureMessageElement.textContent = "It's Hot Today!";
+  } else if (currentTemperature >= 45 && currentTemperature <= 74) {
+    temperatureMessageElement.textContent = "It's Moderate Today!";
+  } else {
+    temperatureMessageElement.textContent = "It's Cold Today!";
+  }
+  afterSearchWeather.appendChild(temperatureMessageElement);
+
+  afterSearchWeather.style.display = "block";
 }
