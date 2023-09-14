2a9b3309f5cfb81e324aedad0b002a049d3ef191
ana <ana@anniecannons.com>
2023-09-14 13:13:31 -0700

    added the future api and the for loop for the api this code is not working but i did get the dates

--- project/script.js
+++ project/script.js
@@ -5,7 +5,7 @@ const afterSearchWeather = document.getElementById("afterSearchWeather");
 
 // Global variables
 let cityName,
-region,
+  region,
   country,
   currentTemperature,
   weatherIcon,
@@ -28,9 +28,8 @@ function inputCityName() {
   });
 }
 
- 
 //  `https://api.weatherapi.com/v1/future.json?key=81c688300b9f4a07b49194332231109&q=${cityName}&dt=${updateDate}`
-// ineedthe next 2 days of the forecast and the futre 2 days so this if you change the date will give me the future 2 days 
+// ineedthe next 2 days of the forecast and the futre 2 days so this if you change the date will give me the future 2 days
 
 // Initialize the event listener
 inputCityName();
@@ -38,21 +37,6 @@ inputCityName();
 // Function to fetch weather data
 async function getWeather(cityName) {
   try {
-    //   const currentResponse = await fetch(
-    //     `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}`
-    //   );
-    
-    //   console.log(currentResponse, "currentResponse ");
-    //   console.log(currentResponse.status);
-    //   if (currentResponse.ok) {
-    //     const currentData = await currentResponse.json();
-    //     console.log(currentData, "currentdata");
-    //     displayCurrentWeather(currentData);
-    //   } else {
-    //     console.error("Failed to fetch current weather data");
-    //   }
-
-    // Fetch forecast data from another API
     const forecastResponse = await fetch(
       `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}`
     );
@@ -65,23 +49,58 @@ async function getWeather(cityName) {
     } else {
       console.error("Failed to fetch weather forecast data");
     }
+
+    // lets get the date for the api
+    // Define the number of days in the future you want to retrieve
+    const numberOfDays = 2;
+
+    // Create an array to store the dates
+    const futureDates = [];
+
+    // Get the current date and time
+    const currentDate = new Date();
+    console.log(currentDate, "currentDate");
+
+    for (let i = 1; i <= numberOfDays; i++) {
+      // Clone the current date to futureDate
+      const futureDate = new Date(currentDate);
+
+      // Calculate the future date by adding 'i' days to futureDate
+      futureDate.setDate(futureDate.getDate() + i);
+
+      // Format the date to YYYY-MM-DD
+      const formattedFutureDate = futureDate.toISOString().split("T")[0];
+      console.log(formattedFutureDate, "formattedFutureDate");
+
+      // Push the formatted future date to the futureDates array
+      futureDates.push(formattedFutureDate);
+
+      // Fetch weather data for each future date using forEach
+      try {
+        const futureResponse = await fetch(
+          `https://api.weatherapi.com/v1/future.json?key=${API_KEY}&q=${cityName}&dt=${formattedFutureDate}`
+        );
+        console.log(
+          `URL for future weather data: https://api.weatherapi.com/v1/future.json?key=${API_KEY}&q=${cityName}&dt=${formattedFutureDate}`
+        );
+
+        console.log(futureResponse, "futureResponse ");
+        console.log(futureResponse.status); // Log the HTTP status code
+        if (futureResponse.ok) {
+          const futureData = await futureResponse.json();
+          console.log(futureData, "futureData");
+          displayFutureWeather(futureData);
+        } else {
+          console.error("Failed to fetch future 2-day forecast data");
+        }
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    }
   } catch (error) {
     console.error("An error occurred:", error);
   }
 }
-
-// Function to display current weather
-// function displayCurrentWeather(currentData) {
-//   if (!currentData) {
-//     return;
-//   }
-//   cityName = currentData.location.name;
-//   console.log(cityName, "cityName");
-//   country = currentData.location.country;
-//   console.log(country, "country");
-//   currentTemperature = Math.floor(currentData.current.temp_f);
-//   console.log(currentTemperature, "currentTemperature");
-// }
 
 // Function to display forecast weather
 function displayForecastWeather(forecastData) {
@@ -140,4 +159,11 @@ function displayForecastWeather(forecastData) {
   afterSearchWeather.appendChild(temperatureMessageElement);
 
   afterSearchWeather.style.display = "block";
+}
+
+// Function to display future 2-day weather
+function displayFutureWeather(futureData) {
+  // Access and display the data for the next 2 days from futureData
+  // You can format the display as per your requirements
+  console.log("Future 2-Day Forecast Data:", futureData);
 }
